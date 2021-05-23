d3953c2f7472ad942912ccf14da5c5406d42e108
Julian Weise <privat@julianweise.de>
2021-05-23 19:33:24 +0200

    Adapt STATIC_ROOT

--- firefighter_self_service/settings.py
+++ firefighter_self_service/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
